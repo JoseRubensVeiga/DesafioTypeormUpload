3db38fefc04c9640a0b069b48735e418b2978097
José <jose.rubens.veiga@gmail.com>
2021-01-27 21:00:24 -0300

    criando os modelos

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,35 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @ManyToOne(() => Category)
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
